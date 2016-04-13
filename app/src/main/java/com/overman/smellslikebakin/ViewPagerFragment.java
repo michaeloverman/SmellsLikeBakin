63b6b527cf37ff39c5f6daa5807ffe0a5dc1af94
michaeloverman <overman.michael@gmail.com>
2016-04-12 23:21:06 -0400

    fragments working

---
+++ app/src/main/java/com/overman/smellslikebakin/ViewPagerFragment.java
@@ -0,0 +1,32 @@
+package com.overman.smellslikebakin;
+
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Michael on 4/12/2016.
+ */
+public class ViewPagerFragment extends Fragment {
+
+    public static final String KEY_RECIPE_INDEX = "recipe index";
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        int index = getArguments().getInt(KEY_RECIPE_INDEX);
+        getActivity().setTitle(Recipes.names[index]);
+        View view = inflater.inflate(R.layout.frament_view_pager, container, false);
+        return view;
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        getActivity().setTitle(getResources().getString(R.string.app_name));
+    }
+}
