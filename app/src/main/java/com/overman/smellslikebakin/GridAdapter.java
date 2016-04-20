ebb67720239e0cc6e9c16e1c8d965584d90c1d9a
michaeloverman <overman.michael@gmail.com>
2016-04-19 22:12:42 -0400

    Finished Android Fragments course on TreeHouse

--- app/src/main/java/com/overman/smellslikebakin/GridAdapter.java
+++ app/src/main/java/com/overman/smellslikebakin/GridAdapter.java
@@ -3,5 +3,20 @@ package com.overman.smellslikebakin;
 /**
  * Created by Michael on 4/19/2016.
  */
-public class GridAdapter {
+public class GridAdapter extends RecycleAdapter {
+    private final GridFragment.OnRecipeSelectedInterface mListener;
+
+    public GridAdapter(GridFragment.OnRecipeSelectedInterface listener) {
+        mListener = listener;
+    }
+
+    @Override
+    protected int getLayoutId() {
+        return R.layout.grid_item;
+    }
+
+    @Override
+    protected void onRecipeSelected(int index) {
+        mListener.onGridRecipeSelected(index);
+    }
 }
--- app/src/main/java/com/overman/smellslikebakin/GridFragment.java
+++ app/src/main/java/com/overman/smellslikebakin/GridFragment.java
@@ -4,8 +4,8 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.GridLayoutManager;
-import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.DisplayMetrics;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -29,6 +29,9 @@ public class GridFragment extends Fragment {
         RecyclerView recyclerView = (RecyclerView) view.findViewById(R.id.RecyclerView);
         GridAdapter gridAdapter = new GridAdapter(listener);
         recyclerView.setAdapter(gridAdapter);
+        DisplayMetrics displayMetrics = getResources().getDisplayMetrics();
+        float dpWidth = displayMetrics.widthPixels / displayMetrics.density;
+        int numColumns = (int) (dpWidth / 200);
         RecyclerView.LayoutManager layoutManager = new GridLayoutManager(getActivity(), numColumns);
         recyclerView.setLayoutManager(layoutManager);
 
